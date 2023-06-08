cd22614d82aeefc84b5159fa120de5f19afbc590
Cyber9ja <ayodejioladapo96@gmail.com>
2023-06-08 17:11:02 +0100

    updated featured top

--- client/src/components/Featured.tsx
+++ client/src/components/Featured.tsx
@@ -4,8 +4,17 @@ import '../sass/featured.scss';
 const FeaturedVehicles = () => {
   return (
     <section id="featured">
-jjulujujjjjjnbnbnbmbnbhbyhhbmnnm n bn
+      <div className="container featured-container">
+        <div className="row justify-content-center">
+          <div className="col-md-12 text-center mb-5">
+            <span className="subheading">What we offer</span>
+            <h2 className="mb-2">Featured Vehicles</h2>
+          </div>
+        </div>
+        <div className="row">
 
+        </div>
+      </div>
     </section>
   )
 }
